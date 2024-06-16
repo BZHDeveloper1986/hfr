e9bccafbedccaf377225daf107834efc633cf552
BZHDeveloper <ledevbreton@gmail.com>
2024-06-16 02:19:19 +0200

    Copie/Colle : version

--- hfr_cc_2.user.js
+++ hfr_cc_2.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @author        BZHDeveloper, roger21
 // @name          [HFR] Copié/Collé v2
-// @version       1.4.35
+// @version       1.4.36
 // @namespace     forum.hardware.fr
 // @description   Colle les données du presse-papiers et les traite si elles sont reconnues.
 // @icon          https://gitlab.gnome.org/BZHDeveloper/HFR/raw/main/hfr-logo.png
