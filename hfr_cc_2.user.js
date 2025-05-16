84ec8c7a5c415fcb9d06736913a81356455a6fea
BZHDeveloper1986 <xavier.gateux@gmx.fr>
2025-05-16 05:35:24 +0200

    Update hfr_cc_2.user.js
    nouvelle adresse pour la table Unicode

--- hfr_cc_2.user.js
+++ hfr_cc_2.user.js
@@ -997,7 +997,7 @@ class Utils {
 			Utils.request({
 				method : "GET",
 				responseType : "json",
-				url : "https://gitlab.gnome.org/BZHDeveloper/hfr/-/raw/main/emojis-data.json",
+				url : "https://github.com/BZHDeveloper1986/hfr/raw/refs/heads/main/emojis-data.json",
 				onload : function (response) {
 					localStorage.setItem ("hfr-cc-data", JSON.stringify (response.response));
 					callback (response.response.unicode_table);
