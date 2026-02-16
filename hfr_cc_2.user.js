71f1ef2ee462b35ef92b40ec86568cc9cdfe903e
BZHDeveloper <xavier.gateux@gmx.fr>
2026-02-16 18:56:40 +0100

    Copie/Colle : numéro de version

--- hfr_cc_2.user.js
+++ hfr_cc_2.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @author        BZHDeveloper, roger21
 // @name          [HFR] Copié/Collé v2
-// @version       1.5.27
+// @version       1.5.28
 // @namespace     forum.hardware.fr
 // @description   Colle les données du presse-papiers et les traite si elles sont reconnues.
 // @icon          https://github.com/BZHDeveloper1986/hfr/blob/main/hfr-logo.png?raw=true
@@ -365,7 +365,7 @@ class Embed {
 								resolve (new Embed ({
 									uri : link,
 									site : site,
-									description : desc
+									description : desc,
 								}));
 							else {
 								Utils.getImageInfo (m.getAttribute ("content")).then (info => {
